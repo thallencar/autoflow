9391371ee3b17cebd34ba9c1c43ce474ab234985
BrunaZorzanello <brunazorzanello@gmail.com>
2026-08-09 20:59:02 -0300

    feat: Criando o estoque e orcamento

---
+++ src/main/java/br/com/autoflow/domain/model/OrcamentoItem.java
@@ -0,0 +1,41 @@
+package com.projeto.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ORCAMENTO_ITENS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrcamentoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_orcamento_item", updatable = false, nullable = false)
+    private UUID id;
+
+    @Builder.Default
+    @Column(name = "st_reserva_estoque", length = 15, nullable = false)
+    private String statusReserva = "Vendido";
+
+    @Column(name = "qt_item", nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "vl_unitario", precision = 10, scale = 2, nullable = false)
+    private BigDecimal valorUnitario;
+
+    @Column(name = "vl_total", precision = 10, scale = 2, nullable = false)
+    private BigDecimal valorTotal;
+
+    @Column(name = "id_estoque", nullable = false)
+    private UUID idEstoque;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_orcamento", nullable = false)
+    private Orcamento orcamento;
+}
---
+++ src/main/java/br/com/autoflow/domain/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.com.autoflow.domain.repository;
+
+import br.com.autoflow.domain.model.Funcionario;
+import br.com.autoflow.domain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    UserDetails findByLogin(String login);
+    Optional<Usuario> findUsuarioByLogin(String login);
+    Optional<Usuario> findByFuncionario (Funcionario funcionario);
+}
